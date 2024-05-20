f20437fef240e11f721b68329b641e2c73c3a3d2
KIGAP <164750787+KIGAP@users.noreply.github.com>
2024-05-20 13:42:22 +0200

    Edded new homework.
    Homework 8 js

---
+++ HW 8 Js/Script/script.js
@@ -0,0 +1,17 @@
+ let number = +prompt('Enter the number');
+ console.log(number**3);
+
+
+ let number1 = +prompt('Enter the number1');
+ let number2 = +prompt('Enter the number2');
+ console.log(number1 + number2);
+
+
+ let user_name = prompt('Enter the username');
+ let user_surname = prompt('Enter the usersurname');
+ let user_age = Number(prompt('Enter the userage'));
+ let user_position = prompt('Enter the userposition');
+ let birth_year = '2024' - user_age;
+ console.log(`Hi, my name is ${user_name} ${user_surname}. 
+ I was born in ${birth_year}. I'm a ${user_position}.`); 
+
